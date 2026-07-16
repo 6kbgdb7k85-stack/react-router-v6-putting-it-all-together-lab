3c8cd0d47976bdef057ceb498be8146fc4dfda95
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-07-16 17:14:13 -0400

    Lab Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-const App = () => {
-    return (
-        <>
-        {/* define all routes here */}
-        </>
-    )
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import DirectorCard from "./pages/DirectorCard";
+import DirectorContainer from "./pages/DirectorContainer";
+import DirectorForm from "./pages/DirectorForm";
+import DirectorList from "./pages/DirectorList";
+import Home from "./pages/Home";
+import MovieCard from "./pages/MovieCard";
+import MovieForm from "./pages/MovieForm";
 
-export default App
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/directors" element={<DirectorContainer />}>
+          <Route path="" element={<DirectorList />} />
+          <Route path="new" element={<DirectorForm />} />
+          <Route path=":id" element={<DirectorCard />}>
+            <Route path="movies/:movieId" element={<MovieCard />} />
+            <Route path="movies/new" element={<MovieForm />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
